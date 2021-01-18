718f80dd995ce72784ad449e48a7bef55d7fb261
hiteple <hiteplegold@gmail.com>
2021-01-18 01:50:28 -0300

    Adding tests

---
+++ tests/mocks/user/user.model.mock.js
@@ -0,0 +1,22 @@
+module.exports = {
+   user: {
+      _id: "507f191e810c19729de860ea",
+      name: "Marluan",
+      username: "Marluan83",
+      password: "myStrongPassword"
+   },
+   users: [
+      {
+         _id: "507f191e810c19729de860ea",
+         name: "Marluan",
+         username: "Marluan83",
+         password: "myStrongPassword2345"
+      },
+      {
+         _id: "507f191e810c19729de236fg",
+         name: "Eric",
+         username: "Eric82",
+         password: "myStrongPassword1234"
+      },
+   ]
+};
